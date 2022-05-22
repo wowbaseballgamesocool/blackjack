29a37a4aa2746e6aeb42076a014c3da747d1d6f0
wowbaseballgamesocool <adambishop555@gmail.com>
2022-05-21 22:49:47 -0400

    basic card totals

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ pygame.init()
 mixer.init()
 pygame.display.set_caption('Blackjack')
 screen = pygame.display.set_mode((1000, 600))
-cards = ['AD', 'AS', 'AC', 'AH', 'KD', 'KS', 'KC', 'KH', 'QS', 'QC', 'QH', 'QD', 'JD', 'JS', 'JC', 'JH', '10D', '10S', '10C', '10H', '9D', '9S', '9C', '9H', '8D', '8S', '8C', '8H', '7D', '7S', '7C', '7H', '6D', '6S', '6C', '6H', '5D', '5S', '5C', '5H', '4D', '4S', '4C', '4H', '3D', '3S', '3C', '3H', '2D', '2S', '2C', '2H', '1D', '1S', '1C', '1H']
-
+cardslist = ['A', 'A', 'A', 'A', 'K', 'K', 'K', 'K', 'Q', 'Q', 'Q', 'Q', 'J', 'J', 'J', 'J', '10', '10', '10', '10', '9', '9', '9', '9', '8', '8', '8', '8', '7', '7', '7', '7', '6', '6', '6', '6', '5', '5', '5', '5', '4', '4', '4', '4', '3', '3', '3', '3', '2', '2', '2', '2', '1', '1', '1', '1']
+blackjack = True
 
 while True:
     
@@ -22,5 +22,27 @@ while True:
 		#if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
         if event.type == pygame.QUIT: exit()
 
-    while blackjack: pass
+    while blackjack:
+        shuffledcards = random.sample(cardslist, len(cardslist))
+        playercards = [shuffledcards[0], shuffledcards[1]]
+        playercount = 0
+        try:
+            playercount += int(shuffledcards[0])
+        except:
+            if shuffledcards[0] in "KQJ": playercount += 10
+            elif shuffledcards[0] in "A":
+                if playercount + 11 > 21: playercount += 1
+                else: playercount += 11
+            
+        try:
+            playercount += int(shuffledcards[1])
+        except:
+            if shuffledcards[1] in "KQJ": playercount += 10
+            elif shuffledcards[1] in "A":
+                if playercount + 11 > 21: playercount += 1
+                else: playercount += 11
+        if shuffledcards[0] == "A" and shuffledcards[1] == "A": playercount = 21
+        print(str(playercards) + "" + str(playercount))
+        
+        
 
